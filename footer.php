b30042142809235c008978b24b70abd28947308e
SHOKI15 <arthur.ferrod@gmail.com>
2020-10-16 11:16:19 +0200

    modif footer

--- footer.php
+++ footer.php
@@ -2,4 +2,8 @@
 <footer>
   <HR NOSHADE>
   <h1>ceci est un footer </h1>
+  <p> suivez nous sur les réseaux sociaux woula </p>
+  <p><h6> a oui c'est vrai on est à pas</h6></p>
+  <p><h5>j'ai codé le site de la nasa et celui d'apple</h5></p>
+  <p><h6> c'est faux :)</h6></p>
 </footer>
